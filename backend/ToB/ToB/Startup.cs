772a161fbb4ac8ef53a4697ac7d00d81512abb81
torte478 <torte478@gmail.com>
2021-02-12 20:36:12 +0300

    feature (*): impl randomizer->get()

--- backend/ToB/ToB/Startup.cs
+++ backend/ToB/ToB/Startup.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using ToB.Interfaces;
 using ToB.Services;
 
@@ -17,6 +11,8 @@ namespace ToB
 {
     public class Startup
     {
+        private const string Policy = "_specificOrigin";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -27,6 +23,14 @@ namespace ToB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o =>
+            {
+                o.AddPolicy(Policy,
+                    p => p.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddTransient<IRandomizer>(_ => new Randomizer(new Random(DateTime.Now.Millisecond)));
@@ -44,6 +48,7 @@ namespace ToB
                 app.UseHsts();
             }
 
+            app.UseCors(Policy);
             app.UseHttpsRedirection();
             app.UseMvc();
         }
--- front/app.js
+++ front/app.js
@@ -4,15 +4,55 @@ Ext.application({
 
     appFolder: 'app',
 
-    controllers: [
-        'Users'
-    ],
+    // controllers: [
+    //     'Users'
+    // ],
 
     launch: function() {
+        var result = {
+            xtype: 'textfield',
+            fieldLabel: 'result',
+            name: 'res'
+        }; 
         Ext.create('Ext.container.Viewport', {
             layout: 'fit',
             items: {
-                    xtype: 'userlist'
+                xtype: 'panel',
+                items: [
+                    {
+                        xtype: 'textfield',
+                        id: 'min',
+                        fieldLabel: 'min',
+                        name: 'min'
+                    },
+                    {
+                        xtype: 'textfield',
+                        id: 'max',
+                        fieldLabel: 'max',
+                        name: 'max'
+                    },
+                    {
+                        xtype: 'button',
+                        text: 'GetRandom',
+                        handler: function () {
+                            var min = Ext.getCmp('min').getValue();
+                            var max = Ext.getCmp('max').getValue();
+
+                            var request = new XMLHttpRequest();
+                            request.open("GET", 'https://localhost:5001/api/randomizer?min=' + min + '&max=' + max, false);
+                            request.send(null);
+                            var response = request.responseText;
+
+                            Ext.getCmp('res').setValue(response);
+                        }
+                    },
+                    {
+                        xtype: 'textfield',
+                        id: 'res',
+                        fieldLabel: 'result',
+                        name: 'res'
+                    }
+                ]
             }
         });
     }
